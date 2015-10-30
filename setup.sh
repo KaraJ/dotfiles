3e13da63305626cc037120a4ae6f56d2868bb11f
Kara Martens <kmartens@wurldtech.com>
2015-10-30 11:24:43 -0700

    Add ackrc fie

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 # Link the files
 ln -s `pwd`/tmux.conf ~/.tmux.conf
 ln -s `pwd`/vimrc ~/.vimrc
+ln -s `pwd`/ackrc ~/.ackrc
 
 # Install NeoBundle
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > neobundle-install.sh
